86c810d735328977baa636f74a17ccaf17510232
furnapso <luke.boland95@gmail.com>
2021-01-15 09:41:16 +1000

    chore: npm & webpack config

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const config = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js'
+    }
+};
+
+module.exports = config;

6a2682ea587847bcc7e0551bb8ed7a512bc1f814
furnapso <luke.boland95@gmail.com>
2021-01-18 18:19:33 +1100

    feat: add home page

---
+++ dist/home.js
@@ -0,0 +1,32 @@
+export const home = (contentDiv) => {
+    contentDiv.innerHTML = `
+    <div class="content-container">
+                <div class="left">
+                    <p>
+                        The Sumo Rose is open from 6pm to 10pm, Tuesday to Saturday. We are closed on Sunday, Monday and on
+                        public holidays.
+                    </p>
+
+                    <p>
+                        Dinner reservations take place across two sittings:
+                        <ul>
+                            <li>
+                                1st session: 6pm to 8pm
+                            </li>
+                            <li>
+                                2nd session: from 8.30pm
+                            </li>
+                        </ul>
+                    </p>
+
+                    <p>
+                        Our omakase menus are priced at $225 per person.
+                    </p>
+
+                    <p>
+                        Reservations are essential and taken one month in advance. We open reservations from 12pm on the
+                        first Tuesday of each month.
+                    </p>
+                </div>
+            </div>`
+}

d75531559be995cdd848cdafd0934b859dad5096
furnapso <luke.boland95@gmail.com>
2021-01-20 20:38:07 +1100

    feat: add devserver config

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,16 @@ const webpack = require('webpack');
 const path = require('path');
 
 const config = {
+    mode: 'development',
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'main.js'
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 9000
     }
 };
 

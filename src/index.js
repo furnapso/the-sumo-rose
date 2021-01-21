ebdb04e817daa21a23dbadfe5f5920d9c660dde2
furnapso <luke.boland95@gmail.com>
2021-01-21 19:18:53 +1100

    feat: add active link logic

--- src/index.js
+++ src/index.js
@@ -4,10 +4,19 @@ import contactPage from "./contact"
 const page = {
     contentDiv: document.querySelector("#content"),
     home: document.querySelector("#home"),
-    contact: document.querySelector("#contact")
+    contact: document.querySelector("#contact"),
+    links: document.querySelectorAll(".menu a")
 }
 
-page.home.addEventListener("click", () => homePage(page.contentDiv));
-page.contact.addEventListener("click", () => contactPage(page.contentDiv));
+page.home.addEventListener("click", () => {
+    page.links.forEach(link => link.classList.remove("active"));
+    page.home.classList.add("active");
+    homePage(page.contentDiv)
+});
+page.contact.addEventListener("click", () => {
+    page.links.forEach(link => link.classList.remove("active"));
+    page.contact.classList.add("active");
+    contactPage(page.contentDiv);
+});
 
 homePage(page.contentDiv);
